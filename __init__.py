1bcd5ae8701fb288bf07a67383eff1ede4c987c0
Gabe Apaza <gapaza@tamu.edu>
2019-12-06 10:52:16 -0600

    some development of connection handler

--- __init__.py
+++ __init__.py
@@ -3,12 +3,36 @@ from mycroft import MycroftSkill, intent_file_handler
 
 class Daphne(MycroftSkill):
     def __init__(self):
-        print("-------> Starting Daphne Skill")
         MycroftSkill.__init__(self)
+        print("Initializing Daphne Skill")
 
-    @intent_file_handler('daphne.intent')
+        # Connection Variables
+        self.connection = None
+        self.session_key = None
+
+
+    def establish_connection(self):
+        print("Establishing connection")
+
+    @intent_file_handler('connect.intent')
     def handle_daphne(self, message):
+        if self.connection is not None:
+            if self.ask_yesno("connection.new.session.query") == "yes":
+                print("Creating a new connection")
+                self.speak_dialog('connection.connecting')
+            else:
+                print("keeping old connection")
+
+        else:
+            print("creating connection")
+            self.connection = 'connected'
+
+
+
         self.speak_dialog('daphne')
+
+
+
 
 
 def create_skill():
